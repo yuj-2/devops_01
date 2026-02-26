d352f1807944eed747ca2b9d3c5d69599e90c4b3
yuj-2 <tree9030@naver.com>
2026-02-26 11:42:35 +0900

    a

---
+++ src/main/java/org/doit/ik/Devops01Application.java
@@ -0,0 +1,13 @@
+package org.doit.ik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops01Application.class, args);
+	}
+
+}
