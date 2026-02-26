f0b44e8c8206c2a7caf5a8f5c9a77265a637cd04
yuj-2 <tree9030@naver.com>
2026-02-26 14:15:19 +0900

    seven

--- src/main/java/org/doit/ik/home/IndexController.java
+++ src/main/java/org/doit/ik/home/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping
+	@GetMapping("/")
 	public String index() {
-		return "/index";
+		return "index";
 	}
 	
 }
